0a26c121da0b9f295f86ed80ce87b8606186c0a0
abhinav-codes123 <abhinavchaudhary484@gmail.com>
2026-07-17 14:21:44 +0530

    github basics

---
+++ ex1.js
@@ -0,0 +1,24 @@
+// var a=10
+// var a=20
+// console.log("a=",a);
+
+// let a=10
+// const b=25
+// console.log("a=",a);
+// console.log("b=",b);
+// b=a++;
+// console.log("b=",b);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log("sum=",sum(10,20));
+
+const sum=(a,b)=> a+b;
+
+console.log("sum=",sum(10,20));
+
+
+
+
+
